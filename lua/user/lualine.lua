01f847893696acf27dc2fae80d86b84e384a90a4
Iain Gray <igray78756@gmail.com>
2022-06-26 15:21:17 -0500

    Updated lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,89 +1,131 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-	return
+  return
 end
 
-local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
-end
-
-local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic" },
-	sections = { "error", "warn" },
-	symbols = { error = " ", warn = " " },
-	colored = false,
-	update_in_insert = false,
-	always_visible = true,
-}
-
-local diff = {
-	"diff",
-	colored = false,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
+vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#859900", bg = "#002b36" })
+vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#eee8d5", bg = "#002b36", bold = false })
+vim.api.nvim_set_hl(0, "SLProgress", { fg = "#eee8d5", bg = "#002b36" })
+vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#839496", bg = "#073642" })
+local mode_color = {
+  n = "#268bd2",
+  i = "#859900",
+  v = "#d33682",
+  [""] = "#d33682",
+  V = "#d33682",
+  c = "#2aa198",
+  no = "#268bd2",
+  s = "#cb4b16",
+  S = "#cb4b16",
+  [""] = "#cb4b16",
+  ic = "#fdf6e3",
+  R = "#dc322f",
+  Rv = "#dc322f",
+  cv = "#268bd2",
+  ce = "##268bd2",
+  r = "#dc322f",
+  rm = "#2aa198",
+  ["r?"] = "#2aa198",
+  ["!"] = "#2aa198",
+  t = "#b58900",
 }
 
 local mode = {
-	"mode",
-	fmt = function(str)
-		return "-- " .. str .. " --"
-	end,
+  -- mode component
+  function()
+    return "▊"
+  end,
+  color = function()
+    -- auto change color according to neovims mode
+    return { fg = mode_color[vim.fn.mode()] }
+  end,
+  -- padding = { right = 1 },
+  padding = 0,
+}
+
+local hide_in_width = function()
+  return vim.fn.winwidth(0) > 80
+end
+
+local icons = require "user.icons"
+
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = icons.diagnostics.Error .. " ", warn = icons.diagnostics.Warning .. " " },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
+}
+
+local diff = {
+  "diff",
+  colored = false,
+  symbols = { added = icons.git.Add .. " ", modified = icons.git.Mod .. " ", removed = icons.git.Remove .. " " }, -- changes diff symbols
+  cond = hide_in_width,
+  separator = "%#SLSeparator#" .. "│ " .. "%*",
 }
 
 local filetype = {
-	"filetype",
-	icons_enabled = false,
-	icon = nil,
+  "filetype",
+  icons_enabled = true,
+  -- icon = nil,
 }
 
 local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "",
+  "branch",
+  icons_enabled = true,
+  icon = "%#SLGitIcon#" .. "" .. "%*" .. "%#SLBranchName#",
+  colored = false,
 }
 
-local location = {
-	"location",
-	padding = 0,
+local progress = {
+  "progress",
+  color = "SLProgress",
 }
 
--- cool function for progress
-local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
+local current_signature = function()
+  if not pcall(require, "lsp_signature") then
+    return
+  end
+  local sig = require("lsp_signature").status_line(30)
+  return "%#SLSeparator#" .. sig.hint .. "%*"
 end
 
-lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, filetype },
-		lualine_y = { location },
-		lualine_z = { progress },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
-})
+local location = {
+  "location",
+  color = function()
+    return { fg = "#002b36", bg = mode_color[vim.fn.mode()] }
+  end,
+}
+
+lualine.setup {
+  options = {
+    globalstatus = true,
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "dashboard" },
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { mode, branch },
+    lualine_b = { diagnostics },
+    lualine_c = { { current_signature, cond = hide_in_width } },
+    lualine_x = { diff, filetype },
+    lualine_y = { progress },
+    lualine_z = { location },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  extensions = {},
+}

081dd306093813f05b1d359c73c3714b6c57ddac
Iain Gray <igray78756@gmail.com>
2022-06-25 22:05:00 -0500

    Updated alpha config with shared icons

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -1,32 +1,34 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+  return
 end
 
-local dashboard = require("alpha.themes.dashboard")
+local icons = require "user.icons"
+
+local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  [[                               __                ]],
+  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":FzfLua files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":FzfProject<CR>"),
-	dashboard.button("r", "  Recently used files", ":FzfLua oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":FzfLua live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+  dashboard.button("f", icons.documents.Files .. " Find file", ":FzfLua files <CR>"),
+  dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("p", icons.git.Repo .. " Find project", ":FzfProject<CR>"),
+  dashboard.button("r", icons.ui.History .. " Recent files", ":FzfLua oldfiles <CR>"),
+  dashboard.button("t", icons.ui.List .. " Find text", ":FzfLua live_grep <CR>"),
+  dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
+  dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
 }
-
 local function footer()
-	local handle = io.popen("fortune vimtips")
-	local fortune = handle:read("*a")
-	handle:close()
-	return fortune
+  local handle = io.popen "fortune vimtips"
+  local fortune = handle:read "*a"
+  handle:close()
+  return fortune
 end
 
 dashboard.section.footer.val = footer()
